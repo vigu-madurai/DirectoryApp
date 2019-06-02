67748e2d1befe3fcbc1a7c6803ab246dace0194d
Vigneshwaran Kannan <vignesh.mountblue@embibe.com>
2019-06-02 23:52:23 +0530

    css fix

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import Home from './modules/Home/index';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <div className='header'/>
+        <Home />
       </header>
     </div>
   );
